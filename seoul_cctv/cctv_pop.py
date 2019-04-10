94bc37166f2bdd3e5b85b1b4a50c1afce6fec3f6
dongjun8983 <dongjun8983@gmail.com>
2019-04-10 18:13:57 +0900

    python 2019-04-10 18:13

---
+++ seoul_cctv/cctv_pop.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+
+ctx = '../data/'
+df_pop = pd.read_excel(ctx + 'population_in_Seoul.xls'
+                       , encoding='UTF-8'
+                       , header=2
+                       , usecols='B,D,G,J,N')
+df_cctv = pd.read_csv(ctx + 'CCTV_in_Seoul.csv')
+
+df_cctv_col = df_cctv.columns
+df_pop__col = df_pop.columns
+
+"""
+df_cctv_col
+Index(['구별', '소계', '2013년도 이전', '2014년', '2015년', '2016년'], dtype='object')
+
+df_pop__col
+Index(['자치구', '계', '계.1', '계.2', '65세이상고령자'],
+      dtype='object')
+
+"""
+
+df_cctv.rename(columns={df_cctv.columns[0]: '구별'}, inplace=True)
+# inplace=True 실제 변수의 내용을 바꿔라
+
+# df_cctv를 소계 기준 오름차순 정렬
+# print(df_cctv.sort_values(by='소계'))
+
+# df_cctv 에서 '2013년도 이전','2014년','2015년','2016년' 열삭제.
+# 결국 df_cctv 는 '구별','소계'만 남김
+df_cctv.drop(['2013년도 이전', '2014년', '2015년', '2016년'], 1, inplace=True)
+
+df_pop.rename(columns={df_pop.columns[0]: '구별'
+    , df_pop.columns[1]: '인구수'
+    , df_pop.columns[2]: '한국인'
+    , df_pop.columns[3]: '외국인'
+    , df_pop.columns[4]: '고령자'}
+              , inplace=True)
+
+# df_pop 에서 0번행 삭제
+df_pop.drop([0], inplace=True)
+
+# df_pop 에서 구별 기준 중복제거
+df_pop['구별'].unique()
+
+# df_pop 에서 null 체크 (null 있는지 여부)
+# print(df_pop[df_pop['구별'].isnull()])
+df_pop.drop([26], inplace=True)
+
+df_pop['외국인비율'] = (df_pop['외국인'] / df_pop['인구수']) * 100
+df_pop['고령자비율'] = (df_pop['고령자'] / df_pop['인구수']) * 100
+
+df_cctv_pop = pd.merge(df_cctv, df_pop, on='구별')
+
+# np.corrcoef()
+
+"""
+r이 -1.0과 -0.7 사이이면, 강한 음적 선형관계,
+r이 -0.7과 -0.3 사이이면, 뚜렷한 음적 선형관계,
+r이 -0.3과 -0.1 사이이면, 약한 음적 선형관계,
+r이 -0.1과 +0.1 사이이면, 거의 무시될 수 있는 선형관계,
+r이 +0.1과 +0.3 사이이면, 약한 양적 선형관계,
+r이 +0.3과 +0.7 사이이면, 뚜렷한 양적 선형관계,
+r이 +0.7과 +1.0 사이이면, 강한 양적 선형관계
+"""
+
+df_cctv_pop.set_index('구별', inplace=True)
+
+cor1 = np.corrcoef(df_cctv_pop['고령자비율'], df_cctv_pop['소계'])
+cor2 = np.corrcoef(df_cctv_pop['외국인비율'], df_cctv_pop['소계'])
+
+#print("고령자비율 상관계수{}, 외국인비율 상관계수 {}".format(cor1, cor2))
+
+df_cctv_pop.to_csv(ctx+'cctv_pop.csv')
---
+++ seoul_cctv/crime_police.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import googlemaps
+
+ctx = '../data/'
+df_crime = pd.read_csv(ctx + 'crime_in_Seoul.csv'
+                       , thousands=','
+                       , encoding='euc-kr')
+gmaps_key = "AIzaSyAdKSloKczDuvOIXj0-7xw_giDkZuAYOw0"
+gmaps = googlemaps.Client(key=gmaps_key)
+# print(df_crime.columns)
+"""
+['관서명', '살인 발생', '살인 검거', '강도 발생',
+ '강도 검거', '강간 발생', '강간 검거', '절도 발생',
+       '절도 검거', '폭력 발생', '폭력 검거']
+"""
+# print(gmaps.geocode('서울중부경찰서', language='ko'))
+
+station_name = []
+for name in df_crime['관서명']:
+    station_name.append('서울' + str(name[:-1]) + '경찰서')
+
+station_addr = []
+station_lat = []  # 위도
+station_lng = []  # 경도
+for name in station_name:
+    tmp = gmaps.geocode(name, language='ko')
+    station_addr.append(tmp[0].get('formatted_address'))
+    tmp_loc = tmp[0].get('geometry')
+    station_lat.append(tmp_loc['location']['lat'])
+    station_lng.append(tmp_loc['location']['lng'])
+    # print(name + '----->' +tmp[0].get('formatted_address'))
+station_name
+station_lat
+station_lng
+gu_names = []
+for name in station_addr:
+    tmp = name.split()
+    tmp_gu = [gu for gu in tmp if gu[-1] == '구'][0]
+    gu_names.append(tmp_gu)
+
+# print(len(df_crime['관서명']))
+df_crime['구별'] = gu_names
+
+# print(df_crime)  #31개의 관서명 존재
+
+# 금천경찰서는관악구 위치에 있어서
+# 금천서를 찾아서 금천구라고 수동 작업
+
+df_crime.loc[df_crime['관서명'] == '금천서', ['구별']] = '금천구'
+
+df_crime.to_csv(ctx + 'crime_police.csv', sep=',', encoding='UTF-8')
+
+df_
